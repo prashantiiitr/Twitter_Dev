5c7cefa415ef0288ce67583da7f45998aa2b6031
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-13 12:58:00 +0530

    Added user model

--- src/index.js
+++ src/index.js
@@ -11,27 +11,9 @@ app.listen(3000,async ()=>{
     console.log("Server started");
     await connect();
     console.log("mongodb connected");
-    //const tweet=await Tweet.create({
-       // content:'Fourth Tweet'
-        
-   // });
-
-  // const tweet=await Tweet.findById('6669379bd98b41c3386e50b0');
-   //tweet.userEmail='b@c.com'
-   //await tweet.save(); 
-
-   
-   const tweetRepo=new TweetRepository();
-   //const tweet=await tweetRepo.get('6669379bd98b41c3386e50b0');
-   //const tweet=tweetRepo.create({content:'my tweet'});
-   //tweet.comment.push({content:'first comment'});
-   //await tweet.save();
-
-
-   //const tweet=tweetRepo.create({content:'Tweet with comment schema'})
-   //const comment=await Comment.create({content:'new comment'});
-   //tweet.comment.push({content:'first comment'});
-   //await tweet.save();
-
-   console.log(tweet);
-})
+    const tweets=await Tweet.find({
+        content:{
+            $all:["First Tweet"]
+        }
+    })
+});
